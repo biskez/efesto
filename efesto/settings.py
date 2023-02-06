82bac7ecf8d040aa443174198ef71128538dac7d
biskez <biskez55@gmail.com>
2023-02-06 21:01:27 +0100

    RELEASE

--- efesto/settings.py
+++ efesto/settings.py
@@ -162,7 +162,7 @@ IS_MONOLINGUAL=False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static') 
 STATIC_URL = '/assets/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'assets'),
